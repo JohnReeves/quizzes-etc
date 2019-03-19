85576a0857ded65f6a9fc1e2262fc87048a382e7
John Reeves <JohnReeves@users.noreply.github.com>
2019-03-19 22:03:37 +0000

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -14,11 +14,19 @@ canvas.create_oval(50,50,450,450,fill="brown")
 # then the tomato topping
 canvas.create_oval(65,65,435,435,fill="red")
 
-
 # draw some more stuff
 # this might be the tomato sauce toppings
 # now add your cheese and anything else, before we get to the pepperoni
 
+colours = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+# create some pepperoni - random 3 places
+for x in range(15):
+    rndX = randint(0,600)
+    rndY = randint(0,400)
+    rndCol = randint(0, (len(colours)-1))
+    canvas.create_oval(rndX,rndY,rndX+50,rndY+50,fill=colours[rndCol])
+    
 # pack the canvas and run the Tk mainloop
 canvas.pack()
 window.mainloop()
